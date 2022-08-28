79d033e90dac3361e3e35ffd9ba8dfa5492cf7f8
GitHub Action <action@github.com>
2022-08-28 08:50:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/krishanunayak/escmapper/services/MyProjectService.kt
+++ src/main/kotlin/com/github/krishanunayak/escmapper/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.krishanunayak.escmapper.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.krishanunayak.escmapper.MyBundle
 
 class MyProjectService(project: Project) {
 
